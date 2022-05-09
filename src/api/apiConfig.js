29cb3ab0ff6eaceecaba4a682113827e2c0e31dd
Inderpreet02 <inder192002@gmail.com>
2022-05-09 23:02:47 +0530

    pattani yaar

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'get from themoviedb.org',
+    apiKey: '26cd3d8a88ad0f8369ba764f4f8c7d53',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
